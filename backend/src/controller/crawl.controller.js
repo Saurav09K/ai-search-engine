24de3069a41e843c3cb8df9c70c1d4145d6ed788
Saurav Kaintura <sauravkaintura49041@gmail.com>
2026-06-01 11:41:29 +0530

    Add User-Agent header to axios request in crawlPage function

--- backend/src/controller/crawl.controller.js
+++ backend/src/controller/crawl.controller.js
@@ -11,8 +11,10 @@ const crawlPage = async (req, res) => {
 
     try {
 
-        const { data } = await axios.get(url);
-
+        const { data } = await axios.get(url, {
+            headers: { 'User-Agent': 'Mozilla/5.0' }
+        });
+        
         const $ = cheerio.load(data);
         $('script, style, nav, header, footer, noscript').remove();
         
